257fd17833754669c7e692300682f6514710f1f7
Stefan Schroeder <stefan.ss.schroeder@siemens.com>
2019-12-22 21:33:54 +0000

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # odtfontfind
 Determine fonts that are used in an ODT LibreOffice document.
+
+http://docs.oasis-open.org/office/v1.2/os/OpenDocument-v1.2-os-part1.html#property-fo_font-family
+
---
+++ odtfontfind.go
@@ -0,0 +1,56 @@
+// Read the fonts from the content.xml of an ODT File
+// to figure out what fonts were used.
+// Stefan Schröder 2019
+package main
+
+import (
+	"archive/zip"
+	"flag"
+	"fmt"
+	"io"
+	"launchpad.net/xmlpath-2"
+	"log"
+)
+
+const xxp = "/document-content/font-face-decls/font-face/@name"
+const file_to_analyze = "content.xml"
+
+func main() {
+	flag.Parse()
+	for _, filename := range flag.Args() {
+		ExampleReader(filename)
+	}
+}
+
+func ExampleReader(fname string) {
+	r, err := zip.OpenReader(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		if f.Name == file_to_analyze {
+			rc, _ := f.Open()
+			parseXML(rc)
+			rc.Close()
+		}
+	}
+}
+
+func parseXML(fd io.Reader) {
+	root, err := xmlpath.Parse(fd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := xmlpath.MustCompile(xxp)
+
+	iter := path.Iter(root)
+	for iter.Next() {
+		strVal := iter.Node().String()
+
+		fmt.Print(strVal)
+		fmt.Print("\n")
+	}
+}
