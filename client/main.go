37a5e7f9986e618112dc1f0a954f6e202d3ef75d
Stefan Schroeder <ondekoza@gmail.com>
2019-12-25 18:10:57 +0100

    Make it a library

---
+++ client/main.go
@@ -0,0 +1,35 @@
+// This is a client program for the odtfindfont library
+// Its purpose is to take some files on the command line
+// and print the font-families that are used within the 
+// file.
+
+// Author: Stefan Schröder
+// License: See LICENSE file.
+package main
+
+import "flag"
+import "fmt"
+import "strings"
+
+import o "github.com/StefanSchroeder/odtfontfind"
+
+func main() {
+	flag.Parse()
+	var ret = []string{}
+	for _, filename := range flag.Args() {
+		if (
+			strings.HasSuffix(filename, ".odt") ||
+			strings.HasSuffix(filename, ".ods") ||
+			strings.HasSuffix(filename, ".odp")  ) {
+			ret = append(ret, o.LibreofficeFontReader(filename)...)
+		} else if (strings.HasSuffix(filename, ".svg")) {
+			ret = append(ret, o.SvgFontReader(filename)...)
+		} else {
+			fmt.Println("Unknown suffix for \"" + filename + "\"")
+		}
+	}
+	for _, item := range ret {
+		fmt.Println(item)
+	}
+}
+
